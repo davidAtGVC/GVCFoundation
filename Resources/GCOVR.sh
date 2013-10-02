1c2c52b28d473482637692d88179f9c665741f63
David Aspinall <daspinall@apple.com>
2013-10-02 09:21:28 -0600

    bunch of tests

--- Resources/GCOVR.sh
+++ Resources/GCOVR.sh
@@ -6,24 +6,45 @@
 PATH=$PATH:$HOME/bin:/Volumes/BUILD/bin:/usr/local/bin:/Applications/Xcode.app/Contents/Developer/usr/bin/
 export PATH
 
-cd ${WORKSPACE}
 source /Volumes/BUILD/Jenkins/xcode_build/${JOB_NAME}/env.sh
 mkdir -p ${WORKSPACE}/gcovr
 
+
+(
 ls -l ${OBJECT_FILE_DIR_normal}
 
 ls -l ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}
 
-(
 /usr/local/bin/gcovr --help
 
 echo
-echo "XML"
-/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o gcovr/coverage.xml
+echo "XML from workspace"
+cd ${WORKSPACE}
+/usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o gcovr/coverage.1.xml
 
 echo
-echo "HTML"
-/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --html -o gcovr/coverage.html
+echo "XML from workspace - relateive"
+cd ${WORKSPACE}
+/usr/local/bin/gcovr -v -r ${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o gcovr/coverage.2.xml
+
+echo
+echo "XML from workspace/JOB_NAME"
+cd ${WORKSPACE}/${JOB_NAME}
+/usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o gcovr/coverage.3.xml
+
+echo
+echo "XML from workspace/JOB_NAME relative"
+cd ${WORKSPACE}/${JOB_NAME}
+/usr/local/bin/gcovr -v -r . --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o gcovr/coverage.4.xml
+
+echo
+echo "XML from object dir"
+cd ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}
+/usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --xml -o gcovr/coverage.5.xml
+
+# echo
+# echo "HTML"
+# /usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --html -o gcovr/coverage.html
 
 ) > /tmp/${JOB_NAME}-gcovr.log 2>&1
 
