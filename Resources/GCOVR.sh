a55650914880b4c88b46f5c7d7939ddfebe02078
David Aspinall <daspinall@apple.com>
2013-10-02 09:27:11 -0600

    Running from object directory rather from source

--- Resources/GCOVR.sh
+++ Resources/GCOVR.sh
@@ -11,40 +11,16 @@ mkdir -p ${WORKSPACE}/gcovr
 
 
 (
-ls -l ${OBJECT_FILE_DIR_normal}
-
-ls -l ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}
-
-/usr/local/bin/gcovr --help
-
-echo
-echo "XML from workspace"
-cd ${WORKSPACE}
-/usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o ${WORKSPACE}/gcovr/coverage.1.xml
-
-echo
-echo "XML from workspace - relateive"
-cd ${WORKSPACE}
-/usr/local/bin/gcovr -v -r ${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o ${WORKSPACE}/gcovr/coverage.2.xml
-
-echo
-echo "XML from workspace/JOB_NAME"
-cd ${WORKSPACE}/${JOB_NAME}
-/usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o ${WORKSPACE}/gcovr/coverage.3.xml
-
-echo
-echo "XML from workspace/JOB_NAME relative"
-cd ${WORKSPACE}/${JOB_NAME}
-/usr/local/bin/gcovr -v -r . --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o ${WORKSPACE}/gcovr/coverage.4.xml
-
-echo
-echo "XML from object dir"
+# /usr/local/bin/gcovr --help
 cd ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}
-/usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --xml -o ${WORKSPACE}/gcovr/coverage.5.xml
 
-# echo
-# echo "HTML"
-# /usr/local/bin/gcovr -v -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --html -o gcovr/coverage.html
+echo
+echo "XML"
+/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --xml -o ${WORKSPACE}/gcovr/coverage.5.xml
+
+echo
+echo "HTML"
+/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --html -o ${WORKSPACE}/gcovr/coverage.5.xml
 
 ) > /tmp/${JOB_NAME}-gcovr.log 2>&1
 
