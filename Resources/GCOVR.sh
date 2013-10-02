bdf9c5e3390b7825617ab1b277c503ebd93eebfb
David Aspinall <daspinall@apple.com>
2013-10-01 18:19:06 -0600

    gcovr script

--- Resources/GCOVR.sh
+++ Resources/GCOVR.sh
@@ -7,6 +7,11 @@ source /Volumes/BUILD/Jenkins/xcode_build/${JOB_NAME}/env.sh
 mkdir -p ${WORKSPACE}/gcovr
 
 /usr/local/bin/gcovr -r ${WORKSPACE} \
-	--object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} \
-	--xml > ${WORKSPACE}/gcovr/coverage.xml
+        --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} \
+        --xml \ 
+        --output ${WORKSPACE}/gcovr/coverage.xml > /tmp/${JOB_NAME}.log 2>&1
 
+/usr/local/bin/gcovr -r ${WORKSPACE} \
+        --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} \
+        --html \ 
+        --output ${WORKSPACE}/gcovr/coverage.html >> /tmp/${JOB_NAME}.log 2>&1
