b3e7938d7f193b900c78f6790ceec3a28d1643b2
David Aspinall <daspinall@apple.com>
2013-10-02 08:41:15 -0600

    path issues

--- Resources/GCOVR.sh
+++ Resources/GCOVR.sh
@@ -10,16 +10,20 @@ cd ${WORKSPACE}
 source /Volumes/BUILD/Jenkins/xcode_build/${JOB_NAME}/env.sh
 mkdir -p ${WORKSPACE}/gcovr
 
+ls -l ${OBJECT_FILE_DIR_normal}
+
+ls -l ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}
+
 (
 /usr/local/bin/gcovr --help
 
 echo
 echo "XML"
-/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --object-directory=${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o gcovr/coverage.xml
+/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o gcovr/coverage.xml
 
 echo
 echo "HTML"
-/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --object-directory=${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --html -o gcovr/coverage.html
+/usr/local/bin/gcovr -r ${WORKSPACE}/${JOB_NAME} --object-directory ${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --html -o gcovr/coverage.html
 
 ) > /tmp/${JOB_NAME}-gcovr.log 2>&1
 
