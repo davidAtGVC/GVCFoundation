2999741046b50384f27a2d6594273c85b65ea763
David Aspinall <daspinall@apple.com>
2013-10-01 19:15:19 -0600

    script

--- Resources/GCOVR.sh
+++ Resources/GCOVR.sh
@@ -7,13 +7,13 @@ source /Volumes/BUILD/Jenkins/xcode_build/${JOB_NAME}/env.sh
 mkdir -p ${WORKSPACE}/gcovr
 
 (
-gcovr --help
+/usr/local/bin/gcovr --help
 
 echo "XML"
-gcovr -r ${WORKSPACE} --object-directory=${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --xml -o ${WORKSPACE}/gcovr/coverage.xml
+/usr/local/bin/gcovr -r "${WORKSPACE}" --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --xml -o "${WORKSPACE}/gcovr/coverage.xml"
 
 echo "HTML"
-gcovr -r ${WORKSPACE} --object-directory=${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH} --html -o ${WORKSPACE}/gcovr/coverage.html
+/usr/local/bin/gcovr -r "${WORKSPACE}" --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --html -o "${WORKSPACE}/gcovr/coverage.html"
 
 ) > /tmp/${JOB_NAME}-gcovr.log 2>&1
 
