c4a2599900dbce401e65ffe408d5c29122856051
David Aspinall <daspinall@apple.com>
2013-10-01 20:09:45 -0600

    added PATH

--- Resources/GCOVR.sh
+++ Resources/GCOVR.sh
@@ -3,6 +3,9 @@
 #  Created by David Aspinall 
 #  Copyright (c) 2011 Global Village Consulting Inc. All rights reserved.
 
+PATH=$PATH:$HOME/bin:/Volumes/BUILD/bin:/usr/local/bin:/Applications/Xcode.app/Contents/Developer/usr/bin/
+export PATH
+
 cd ${WORKSPACE}
 source /Volumes/BUILD/Jenkins/xcode_build/${JOB_NAME}/env.sh
 mkdir -p ${WORKSPACE}/gcovr
@@ -12,11 +15,11 @@ mkdir -p ${WORKSPACE}/gcovr
 
 echo
 echo "XML"
-/usr/local/bin/gcovr -r . --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --xml --gcov-executable=/usr/local/bin/gcovr -o "gcovr/coverage.xml"
+/usr/local/bin/gcovr -r . --object-directory=\"${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}\" --xml --gcov-executable=/usr/local/bin/gcovr -o gcovr/coverage.xml
 
 echo
 echo "HTML"
-/usr/local/bin/gcovr -r . --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --html --gcov-executable=/usr/local/bin/gcovr -o "gcovr/coverage.html"
+/usr/local/bin/gcovr -r . --object-directory=\"${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}\" --html --gcov-executable=/usr/local/bin/gcovr -o gcovr/coverage.html
 
 ) > /tmp/${JOB_NAME}-gcovr.log 2>&1
 
