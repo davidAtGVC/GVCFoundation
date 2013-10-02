93a917669b687fbdf3bb017e65292eea663605ac
David Aspinall <daspinall@apple.com>
2013-10-01 19:20:21 -0600

    Changing to the workspace directory

--- Resources/GCOVR.sh
+++ Resources/GCOVR.sh
@@ -3,6 +3,7 @@
 #  Created by David Aspinall 
 #  Copyright (c) 2011 Global Village Consulting Inc. All rights reserved.
 
+cd ${WORKSPACE}
 source /Volumes/BUILD/Jenkins/xcode_build/${JOB_NAME}/env.sh
 mkdir -p ${WORKSPACE}/gcovr
 
@@ -10,10 +11,10 @@ mkdir -p ${WORKSPACE}/gcovr
 /usr/local/bin/gcovr --help
 
 echo "XML"
-/usr/local/bin/gcovr -r "${WORKSPACE}" --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --xml -o "${WORKSPACE}/gcovr/coverage.xml"
+/usr/local/bin/gcovr -r . --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --xml -o "gcovr/coverage.xml"
 
 echo "HTML"
-/usr/local/bin/gcovr -r "${WORKSPACE}" --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --html -o "${WORKSPACE}/gcovr/coverage.html"
+/usr/local/bin/gcovr -r . --object-directory="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}" --html -o "gcovr/coverage.html"
 
 ) > /tmp/${JOB_NAME}-gcovr.log 2>&1
 
