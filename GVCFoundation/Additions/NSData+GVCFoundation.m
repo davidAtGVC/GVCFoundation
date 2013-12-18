f2ecbcf2c3bb1a45d62d4d7dcaffa080cff8f138
David Aspinall <daspinall@apple.com>
2013-12-18 09:27:39 -0700

    Removed base64 logic in favour of the baked in versions from Apple

--- GVCFoundation/Additions/NSData+GVCFoundation.m
+++ GVCFoundation/Additions/NSData+GVCFoundation.m
@@ -16,33 +16,7 @@ const NSUInteger kDefaultMaxBytesToHexDump = 1024;
 
 @implementation NSData (GVCFoundation)
 
-static unsigned char gvc_b64encodingTable[64] = {
-	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
-	'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
-	'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-	'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'
-};
-
 #define xx 65
-
-static unsigned char gvc_b64decodingTable[256] = {
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, 62, xx, xx, xx, 63,
-	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, xx, xx, xx, xx, xx, xx,
-	xx,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, xx, xx, xx, xx, xx,
-	xx, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx,
-	xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx
-};
 
 - (NSString *)description
 {
@@ -164,113 +138,17 @@ static unsigned char gvc_b64decodingTable[256] = {
 
 - (NSString *)gvc_base64Encoded
 {
-	const unsigned char	*bytes = [self bytes];
-	NSMutableString *result = [NSMutableString stringWithCapacity:[self length]];
-	unsigned long ixtext = 0;
-	unsigned long lentext = [self length];
-	long ctremaining = 0;
-	unsigned char inbuf[3], outbuf[4];
-	unsigned short i = 0;
-	unsigned short charsonline = 0, ctcopy = 0;
-	unsigned long ix = 0;
-	
-	while( YES )
-	{
-		ctremaining = (long)(lentext - ixtext);
-		if( ctremaining <= 0 ) break;
-		
-		for( i = 0; i < 3; i++ ) {
-			ix = ixtext + i;
-			if( ix < lentext ) inbuf[i] = bytes[ix];
-			else inbuf [i] = 0;
-		}
-		
-		outbuf [0] = (inbuf [0] & 0xFC) >> 2;
-		outbuf [1] = ((inbuf [0] & 0x03) << 4) | ((inbuf [1] & 0xF0) >> 4);
-		outbuf [2] = ((inbuf [1] & 0x0F) << 2) | ((inbuf [2] & 0xC0) >> 6);
-		outbuf [3] = inbuf [2] & 0x3F;
-		ctcopy = 4;
-		
-		switch( ctremaining )
-		{
-			case 1:
-				ctcopy = 2;
-				break;
-			case 2:
-				ctcopy = 3;
-				break;
-		}
-		
-		for( i = 0; i < ctcopy; i++ )
-			[result appendFormat:@"%c", gvc_b64encodingTable[outbuf[i]]];
-		
-		for( i = ctcopy; i < 4; i++ )
-			[result appendString:@"="];
-		
-		ixtext += 3;
-		charsonline += 4;
-	}
-	
-	return [NSString stringWithString:result];
+    return [self base64EncodedStringWithOptions:0];
 }
 
 + (NSData *)gvc_Base64Decoded:(NSString *)encoded
 {
-	return [[encoded dataUsingEncoding:NSUTF8StringEncoding] gvc_base64Decoded];
+    return [[NSData alloc] initWithBase64EncodedString:encoded options:(NSDataBase64DecodingOptions)0];
 }
 
 - (NSData *)gvc_base64Decoded
 {
-	const unsigned char	*bytes = [self bytes];
-	NSMutableData *result = [NSMutableData dataWithCapacity:((([self length] + 3) / 4) * 3)];
-
-	NSUInteger length = [self length];
-    unsigned char ch;
-	size_t i = 0;
-	while (i < length)
-	{
-		unsigned char inbuf[4];
-		size_t inindex = 0;
-		while (i < length)
-		{
-			unsigned char decode = gvc_b64decodingTable[bytes[i++]];
-			if (decode != xx)
-			{
-				inbuf[inindex] = decode;
-				inindex++;
-				
-				if (inindex == 4)
-				{
-					break;
-				}
-			}
-		}
-		
-		if (inindex == 0)
-			break;
-		if (inindex == 1)
-		{
-			//  At least two characters are needed to produce one byte!
-			return nil;
-		}
-
-		if (inindex >= 2)
-		{
-			ch = (inbuf[0] << 2) | (inbuf[1] >> 4);
-			[result appendBytes:&ch length:1];
-		}
-		if (inindex >= 3)
-		{
-			ch = (inbuf[1] << 4) | (inbuf[2] >> 2);
-			[result appendBytes:&ch length:1];
-		}
-		if (inindex >= 4)
-		{
-			ch = (inbuf[2] << 6) | inbuf[3];
-			[result appendBytes:&ch length:1];
-		}
-	}
-	return [NSData dataWithData:result];
+    return [[NSData alloc] initWithBase64EncodedData:self options:(NSDataBase64DecodingOptions)0];
 }
 
 
